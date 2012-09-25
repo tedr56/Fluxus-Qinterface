a2a722a58a5268d268ee9b98db8f3faee89673d3
VJ T <ted.r@hotmail.fr>
2012-09-25 15:41:30 +0200

    Adding MediaWidget - DnD Widget for MediaControl

--- src/mediawidget.cpp
+++ src/mediawidget.cpp
@@ -1,23 +1,69 @@
+#include <QFileInfo>
+#include <QSpacerItem>
 #include "mediawidget.h"
+#include "mediacontrol.h"
 #include "multicontrol.h"
 #include "buttoncontrol.h"
 
+StackHBoxLayout::StackHBoxLayout(QWidget *parent) : QBoxLayout(QBoxLayout::LeftToRight, parent)
+{
+    setSizeConstraint(QLayout::SetMaximumSize);
+    setContentsMargins(0,0,0,0);
+    setSpacing(2);
+    addSpacerItem(new QSpacerItem(10, 10, QSizePolicy::Expanding, QSizePolicy::MinimumExpanding));
+}
+
+void StackHBoxLayout::addWidget(QWidget *w, int stretch = 0 , Qt::Alignment align = Qt::AlignLeft)
+{
+    insertWidget(count() - 1, w, stretch, align);
+}
+
 MediaWidget::MediaWidget(QWidget *parent) : QWidget(parent)
 {
-    m_mimeType = "multicontrol/media";
-    QHBoxLayout* MediaLayout = new QHBoxLayout(this);
-    MediaLayout->setSizeConstraint(QLayout::SetMinimumSize);
-    MediaLayout->setContentsMargins(0,0,0,0);
-    MediaLayout->setSpacing(0);
+    m_mimeTypes = QStringList("fluxus/visual");
+    m_layout = new StackHBoxLayout(this);
     setAcceptDrops(true);
     setBackgroundRole(QPalette::Dark);
-    setLayout(MediaLayout);
+    setLayout(m_layout);
+}
+
+void MediaWidget::setMimeType(QStringList Type)
+{
+    m_mimeTypes = Type;
+}
+
+QStringList MediaWidget::MimeType()
+{
+    return m_mimeTypes + m_externalMimeTypes;
+}
+
+void MediaWidget::setParametersDock(QWidget *paramDock)
+{
+    m_parametersDock = paramDock;
+}
+
+bool MediaWidget::checkMimeFormat(QStringList mimeFormats)
+{
+    for (int i = 0; i < mimeFormats.size(); i++){
+        if (MimeType().contains(mimeFormats.at(i)))
+            return true;
+    }
+    return false;
+}
+
+QStringList MediaWidget::getValidMimeFormats(QStringList mimeFormats)
+{
+    QStringList validFormats;
+    foreach(const QString &str, mimeFormats){
+        if (MimeType().contains(str))
+            validFormats.append(str);
+    }
+    return validFormats;
 }
 
 void MediaWidget::dragEnterEvent(QDragEnterEvent* event)
 {
-    qDebug() << "DragEnter Debug " << event->mimeData()->formats() << " " << m_mimeType;
-    if (event->mimeData()->hasFormat(m_mimeType)) {
+    if (checkMimeFormat(event->mimeData()->formats())) {
         if (children().contains(event->source())) {
             event->setDropAction(Qt::MoveAction);
             event->accept();
@@ -31,10 +77,7 @@ void MediaWidget::dragEnterEvent(QDragEnterEvent* event)
 
 void MediaWidget::dragMoveEvent(QDragMoveEvent *event)
 {
-    qDebug() << "DragMove Debug ";
-    qDebug() << event->mimeData()->formats() << " " << m_mimeType;
-    qDebug() << event->source();
-    if (event->mimeData()->hasFormat(m_mimeType)) {
+    if (checkMimeFormat(event->mimeData()->formats())) {
         if (children().contains(event->source())) {
             event->setDropAction(Qt::MoveAction);
             event->accept();
@@ -48,58 +91,94 @@ void MediaWidget::dragMoveEvent(QDragMoveEvent *event)
 
 void MediaWidget::dropEvent(QDropEvent *event)
 {
-    qDebug() << "Drop Debug " << event->mimeData()->formats() << " " << m_mimeType;
-    if (event->mimeData()->hasFormat(m_mimeType)) {
-        const QMimeData *mime = event->mimeData();
-        QByteArray itemData = mime->data(m_mimeType);
-        QDataStream dataStream(&itemData, QIODevice::ReadOnly);
-        int channel;
-        int control;
-        dataStream >> channel >> control;
-        qDebug() << channel << " " << control;
-        ButtonControl* Target = new ButtonControl(this);
-//        MediaLayout->addWidget(Target);
-        Target->setChannel(channel);
-        Target->setControl(control);
-        Target->setOutputMode(MultiControl::OutMidi);
-        Target->setInputMode(MultiControl::MidiCC);
-        Target->setMinimumSize(60,60);
-        layout()->addWidget(Target);
-        if (event->source() == this) {
+    if (checkMimeFormat(event->mimeData()->formats())) {
+        if (event->source()->parent() == this) {
             event->setDropAction(Qt::MoveAction);
             event->accept();
+            dropAction(event);
         } else {
             event->acceptProposedAction();
+            dropAction(event);
         }
     } else {
         event->ignore();
     }
 }
 
-void MediaWidget::mousePressEvent(QMouseEvent *event)
-{
-    if (event->button() == Qt::LeftButton){
-        MultiControl *child = static_cast<MultiControl *>(childAt(event->pos()));
-        if (!child)
-            return;
-        QPoint hotspot = event->pos() - child->pos();
-
-        QByteArray itemData;
-        QDataStream dataStream(&itemData, QIODevice::WriteOnly);
-        dataStream << child->getChannel() << child->getControl();
-
-        QMimeData *mimeData = new QMimeData;
-        mimeData->setData(m_mimeType, itemData);
-
-        QDrag *drag = new QDrag(this);
-        drag->setMimeData(mimeData);
-        drag->setHotSpot(hotspot);
-
-        child->hide();
-
-        if (drag->exec(Qt::MoveAction | Qt::CopyAction, Qt::CopyAction) == Qt::MoveAction)
-            child->close();
-        else
-            child->show();
+void MediaWidget::dropAction(QDropEvent *event){
+    const QMimeData *mime = event->mimeData();
+    if (mime->formats().contains("fluxus/visual")){
+        QByteArray itemData = mime->data("fluxus/visual");
+        QDataStream dataStream(&itemData, QIODevice::ReadOnly);
+        QString filePath;
+        int defValues;
+        dataStream >> filePath >> defValues;
+        FluxusVisual* Target = new FluxusVisual(filePath, this, defValues);
+        m_layout->addWidget(Target);
     }
+}
+
+void MediaWidget::addWidget(QWidget *w)
+{
+    m_layout->insertWidget(m_layout->count() - 1, w, 1, Qt::AlignLeft);
+}
+
+DeckWidget::DeckWidget(QWidget *parent) : MediaWidget(parent)
+{
+    m_externalMimeTypes.append("text/plain");
+    setAcceptDrops(true);
+}
+
+QStringList DeckWidget::getFilesPath(QList<QUrl> urlList)
+{
+    QStringList pathList;
+    for (int i = 0; i < urlList.size() && i < 32; ++i)
+    {
+        pathList.append(urlList.at(i).toLocalFile());
+    }
+    return pathList;
+}
+
+void DeckWidget::dropEvent(QDropEvent *event)
+{
+    if (checkMimeFormat(event->mimeData()->formats())) {
+        if (event->source()){
+            if (event->source()->parent() == this) {
+                event->setDropAction(Qt::MoveAction);
+                event->accept();
+            }
+        } else {
+            event->acceptProposedAction();
+            dropAction(event);
+        }
+    } else {
+        event->ignore();
+    }
+}
+
+void DeckWidget::dropAction(QDropEvent *event)
+{
+    QStringList validMime = getValidMimeFormats(event->mimeData()->formats());
+    int externalMedia = 0;
+    externalMedia += addFluxusMedia(event);
+    if (!externalMedia)
+        MediaWidget::dropEvent(event);
+}
+
+int DeckWidget::addFluxusMedia(QDropEvent *event)
+{
+    int addedMedia = 0;
+    if ((event->mimeData()->formats().contains("text/plain")) && (event->mimeData()->hasUrls())){
+        QStringList filesPath = getFilesPath(event->mimeData()->urls());
+        foreach(const QString &path, filesPath){
+            QFileInfo fileInfo(path);
+            if (fileInfo.completeSuffix() == "scm"){
+                FluxusVisual* FlxsVisual = new FluxusVisual(fileInfo, this);
+                m_layout->addWidget(FlxsVisual);
+                //connect()
+                addedMedia++;
+            }
+        }
+    }
+    return addedMedia;
 }
--- src/mediawidget.h
+++ src/mediawidget.h
@@ -8,26 +8,66 @@
 class QDragEnterEvent;
 class QDropEvent;
 
+class StackHBoxLayout : public QBoxLayout
+{
+    Q_OBJECT
+public:
+    StackHBoxLayout(QWidget* parent = 0);
+    void addWidget(QWidget *, int stretch, Qt::Alignment alignment);
+};
+
 class MediaWidget : public QWidget
 {
     Q_OBJECT
 
 public:
-    MediaWidget(QWidget* parent = 0);
-    void setMimeType(QString& Type) { m_mimeType=Type; } ;
-    QString MimeType() { return m_mimeType; };
-
+    MediaWidget(QWidget* parent);
+    QStringList MimeType();
+    void setParametersDock(QWidget*);
 protected:
+    void setMimeType(QStringList);
     void dragEnterEvent(QDragEnterEvent* event);
     void dragMoveEvent(QDragMoveEvent *event);
     void dropEvent(QDropEvent *event);
-    void mousePressEvent(QMouseEvent *event);
+    void dropAction(QDropEvent *event);
+    //void mousePressEvent(QMouseEvent *event);
 
+    bool checkMimeFormat(QStringList);
+    QStringList getValidMimeFormats(QStringList);
+
+    QStringList m_mimeTypes;
+    QStringList m_externalMimeTypes;
+
+    StackHBoxLayout* m_layout;
+    void addWidget(QWidget* w);
+
+    QWidget* m_parametersDock;
 private:
-    QHBoxLayout *MediaLayout;
-    QString m_mimeType;
     Qt::DropAction DragFlag;
     Qt::DropActions DropFlags;
+};
+
+
+class DeckWidget : public MediaWidget
+{
+    Q_OBJECT
+public:
+    DeckWidget(QWidget* parent);
+    QStringList MimeType();
+protected:
+    void dropEvent(QDropEvent *event);
+    void dropAction(QDropEvent *event);
+private:
+    QStringList getFilesPath(QList<QUrl>);
+    int addFluxusMedia(QDropEvent *event);
+};
+
+class SideWidget : public MediaWidget
+{
+    Q_OBJECT
+public:
+    SideWidget(QWidget *parent = 0);
+protected:
 };
 
 #endif
